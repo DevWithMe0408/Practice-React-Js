5a9f162ff098c7622ba0766372eba64a3970a721
DevWithMe0408 <dinhchientv03@gmail.com>
2025-07-12 16:54:19 +0700

    Complete #30 JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,14 @@
-
+import './style.css'
 const MyComponent = () => {
     return (
-        <div>
-            <h2>My Component</h2>
-        </div>
+        <>
+            <div
+                style={{ color: 'red' }} >
+                <h2>My Component</h2>
+            </div>
+            <div className="child"> Child</div>
+        </>
+
     );
 }
 export default MyComponent;
