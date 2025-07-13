e5458bf9b744bc3d37c66720a0bc11afb326b048
DevWithMe0408 <dinhchientv03@gmail.com>
2025-07-13 11:39:45 +0700

    Complete #31 data types

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,16 @@
 import './style.css'
 const MyComponent = () => {
+    const stringdata = 'Chien dep trai'; // string type
+    const numberdata = 22; // number type
+    const booleandata = true; // boolean type
+    const objectdata = { name: 'Chien', age: 22 }; // object type
+    const arraydata = ['Chien', 'dep', 'trai']; // array type
     return (
         <>
+            <div> {stringdata} {numberdata} age</div>
+            <p>{booleandata}</p>
+            <p>{JSON.stringify(objectdata)}</p>
+            <p>{arraydata}</p>
             <div
                 style={{ color: 'red' }} >
                 <h2>My Component</h2>
